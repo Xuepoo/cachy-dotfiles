35a140882c43631ccbfd6e71d28e833e380a2ccb
oodenX <ven3428set@163.com>
2026-04-14 14:06:47 +0800

    chore: add env variables

--- .config/fish/conf.d/environment_variables.fish
+++ .config/fish/conf.d/environment_variables.fish
@@ -2,6 +2,10 @@ set -x RUSTUP_UPDATE_ROOT https://mirrors.tuna.tsinghua.edu.cn/rustup/up
 set -x RUSTUP_DIST_SERVER https://mirrors.tuna.tsinghua.edu.cn/rustup
 set -x HF_ENDPOINT https://hf-mirror.com
 
+set -gx EDITOR nvim
+set -gx VISUAL nvim
+set -gx SYSTEMD_EDITOR nvim
+
 set -x PNPM_HOME $HOME/.local/share/pnpm
 fish_add_path $HOME/.local/share/npm-global/bin
 fish_add_path $PNPM_HOME
