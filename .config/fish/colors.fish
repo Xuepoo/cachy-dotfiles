fc552ea357ad5e71cbc9a996dcd4e093ef6a73b4
oodenX <ven3428set@163.com>
2026-05-22 00:14:51 +0800

    refactor(hypr): relocate togglemode and mpris_notifier scripts to ~/.config/hypr/scripts

--- .config/fish/colors.fish
+++ .config/fish/colors.fish
@@ -1,23 +1,23 @@
 # ~/.config/fish/colors-template.fish
 # Fish syntax highlighting colors compiled by Matugen
 
-set -U fish_color_normal '#e0e2ef'
-set -U fish_color_command '#b1c5ff' --bold
-set -U fish_color_param '#c4c3eb'
-set -U fish_color_keyword '#cfbef6' --bold
-set -U fish_color_quote '#cfbef6'
-set -U fish_color_redirection '#b1c5ff'
-set -U fish_color_end '#c4c3eb'
+set -U fish_color_normal '#dde3ee'
+set -U fish_color_command '#a1c9ff' --bold
+set -U fish_color_param '#bbc6ea'
+set -U fish_color_keyword '#c2c1f9' --bold
+set -U fish_color_quote '#c2c1f9'
+set -U fish_color_redirection '#a1c9ff'
+set -U fish_color_end '#bbc6ea'
 set -U fish_color_error '#ffb4ab' --bold
-set -U fish_color_comment '#c4c6d3'
-set -U fish_color_selection --background='#292c36'
-set -U fish_color_search_match --background='#292c36'
-set -U fish_color_operator '#b1c5ff'
-set -U fish_color_escape '#cfbef6'
-set -U fish_color_autosuggestion '#585b66'
+set -U fish_color_comment '#c1c7d2'
+set -U fish_color_selection --background='#272d36'
+set -U fish_color_search_match --background='#272d36'
+set -U fish_color_operator '#a1c9ff'
+set -U fish_color_escape '#c2c1f9'
+set -U fish_color_autosuggestion '#565c66'
 
 # Fish pager colors
-set -U fish_pager_color_prefix '#b1c5ff' --bold
-set -U fish_pager_color_completion '#e0e2ef'
-set -U fish_pager_color_description '#c4c6d3'
-set -U fish_pager_color_progress '#c4c3eb'
+set -U fish_pager_color_prefix '#a1c9ff' --bold
+set -U fish_pager_color_completion '#dde3ee'
+set -U fish_pager_color_description '#c1c7d2'
+set -U fish_pager_color_progress '#bbc6ea'
