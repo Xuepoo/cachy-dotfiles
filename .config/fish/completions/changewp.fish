88af93d2c5e8a3ac2cd59687bd6a97cf404249a5
oodenX <ven3428set@163.com>
2026-05-21 21:50:10 +0800

    feat(theme): implement dynamic btop theme, cava gradient, and clean up legacy noctalia configs

--- .config/fish/completions/changewp.fish
+++ .config/fish/completions/changewp.fish
@@ -1,3 +1,3 @@
 # ~/.config/fish/completions/changewp.fish
 complete -c changewp -f
-complete -c changewp -a "(ls -1 ~/Pictures/Wallpapers/ | string match -r '.*\.(jpg|png|jpeg)')" -d "Wallpaper"
+complete -c changewp -a "(find ~/Pictures/Wallpapers/ -type f \( -name '*.jpg' -o -name '*.png' -o -name '*.jpeg' \) | string replace \"$HOME/Pictures/Wallpapers/\" \"\")" -d "Wallpaper"
