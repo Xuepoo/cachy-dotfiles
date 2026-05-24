3b3d8095661924463b2fe76483eaee382635035a
Xuepoo <xuepoofoter@gmail.com>
2026-05-24 20:25:52 +0800

    style(theme): update generated dynamic wallpaper theme configuration

--- .config/fish/colors.fish
+++ .config/fish/colors.fish
@@ -1,23 +1,23 @@
 # ~/.config/fish/colors-template.fish
 # Fish syntax highlighting colors compiled by Matugen
 
-set -U fish_color_normal '#dfe2ef'
-set -U fish_color_command '#aac7ff' --bold
-set -U fish_color_param '#c0c4eb'
-set -U fish_color_keyword '#c9bff8' --bold
-set -U fish_color_quote '#c9bff8'
-set -U fish_color_redirection '#aac7ff'
-set -U fish_color_end '#c0c4eb'
+set -U fish_color_normal '#f9ddd3'
+set -U fish_color_command '#ffb599' --bold
+set -U fish_color_param '#f0bc95'
+set -U fish_color_keyword '#efbe79' --bold
+set -U fish_color_quote '#efbe79'
+set -U fish_color_redirection '#ffb599'
+set -U fish_color_end '#f0bc95'
 set -U fish_color_error '#ffb4ab' --bold
-set -U fish_color_comment '#c3c6d3'
-set -U fish_color_selection --background='#282d36'
-set -U fish_color_search_match --background='#282d36'
-set -U fish_color_operator '#aac7ff'
-set -U fish_color_escape '#c9bff8'
-set -U fish_color_autosuggestion '#575b66'
+set -U fish_color_comment '#dbc1b8'
+set -U fish_color_selection --background='#392923'
+set -U fish_color_search_match --background='#392923'
+set -U fish_color_operator '#ffb599'
+set -U fish_color_escape '#efbe79'
+set -U fish_color_autosuggestion '#6b5750'
 
 # Fish pager colors
-set -U fish_pager_color_prefix '#aac7ff' --bold
-set -U fish_pager_color_completion '#dfe2ef'
-set -U fish_pager_color_description '#c3c6d3'
-set -U fish_pager_color_progress '#c0c4eb'
+set -U fish_pager_color_prefix '#ffb599' --bold
+set -U fish_pager_color_completion '#f9ddd3'
+set -U fish_pager_color_description '#dbc1b8'
+set -U fish_pager_color_progress '#f0bc95'
